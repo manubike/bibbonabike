c01e24ad52dcc034ae42a52787798aa767a7b35a
ZonkeyNet <zonkeynet@gmail.com>
2025-05-25 17:50:06 +0200

    some minor change TA
    need adjust the form price

---
+++ js/nav.js
@@ -0,0 +1,39 @@
+    // NAV JS
+    function toggleMenu() {
+
+      const nav = document.querySelector('nav ul');
+      const header = document.getElementById('header');
+      const btn = document.getElementById('menuBtn');
+  
+      nav.classList.toggle('show');
+      header.classList.toggle('menu-open');
+  
+      // Cambia icona
+      if (btn.textContent === "☰") {
+        btn.textContent = "✖";
+      } else {
+        btn.textContent = "☰";
+      }
+    }
+  
+    // Logo effetto pulse opzionale
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', () => {
+      logo.classList.toggle('pulse');
+    });
+    window.addEventListener("DOMContentLoaded", () => {
+      setTimeout(() => {
+        document.querySelector('.hero-content').classList.add('show');
+      }, 400); // Delay in ms
+    });
+
+    // Scroll fluido + scroll-to-form
+    document.querySelectorAll('.scroll-btn').forEach(btn => {
+      btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
---
+++ js/slogan.js
@@ -0,0 +1,39 @@
+    const slogans = [
+      "Pedala la libertà tra i sentieri toscani",
+      "Scopri la Toscana. Una pedalata alla volta.",
+      "Dove finisce l’asfalto, inizia l’avventura",
+      "Bike. Relax. Officina. Tutto in un solo posto.",
+      "Magona Bike Garage: la tua sella in Toscana",
+      "Bibbona su due ruote, 24 ore su 24"
+    ];
+  
+    const title = document.getElementById("slideshow-title");
+    let index = 0;
+  
+    function showNextSlogan() {
+      title.classList.remove("visible");
+      setTimeout(() => {
+        index = (index + 1) % slogans.length;
+        title.textContent = slogans[index];
+        title.classList.add("visible");
+      }, 500);
+    }
+  
+    title.classList.add("visible");
+    setInterval(showNextSlogan, 4000);
+
+      const phrases = [
+        "Chi siamo?",
+        "Appassionati di bici, natura e avventura.",
+        "Magona Bike Garage è più di un'officina.",
+        "È un punto di ritrovo per chi ama pedalare tra i paesaggi toscani."
+    ];
+
+    const container = document.getElementById("footerCredits");
+
+    phrases.forEach((text, i) => {
+        const p = document.createElement("p");
+        p.textContent = text;
+        p.style.animationDelay = `${i * 2}s`;
+        container.appendChild(p);
+    });
